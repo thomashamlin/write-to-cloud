3054daf6dd7e69cb73626022e23a2171f1ed5568
Thomas Hamlin <thomas@cloudboltsoftware.com>
2014-10-22 20:18:41 -0700

    Basic text insertion, deletion, and cursor movement

--- js/type-resource.js
+++ js/type-resource.js
@@ -3,83 +3,174 @@
 window.type = (function() {
   'use strict';
 
+  // Identifiers
   var contentKey = 'type-project.content';
+  var cursorKey = 'type-project.cursor';
   var contentSelector = '#content';
 
+  // Some key codes
+  var BACK_SPACE = 8;
+  var DELETE = 46;
+  var ENTER = 13;
+  var SPACE = 32;
+  var SHIFT = 16;
+  var LEFT = 37;
+  var RIGHT = 39;
+  var CURSOR = "\u2038";
+  var LS = "\u2028"; // unicode line separator
+  var PS = "\u2029"; // unicode paragraph separator
 
-  /* Draw the string `content` to the content area.
-   * This function will translate markdown into HTML eventually.
+
+  /* Re-draw the stored content to the content area.
    */
-  function drawContent(content) {
-    if (content) {
-      $(contentSelector).html(content);
-    //} else {
-    //  $('#content').html('<span class="placeholder">Type here</span>');
-    }
+  function refreshContent() {
+    var html = plainToHtml(getContent());
+    $(contentSelector).html(html);
   }
 
 
-  /* Return the string contents stored in localStorage, or null.
-   * Uses the localStorage key defined for this project.
+  /* Return HTML representation of content.
    */
-  function loadContent() {
+  function plainToHtml(plain) {
+    // convert to lines
+    var lines = plain.split(LS);
+    var html = '';
+    var line;
+
+    for (var i = 0; i < lines.length; i++) {
+      line = (lines[i] === "") ? "&nbsp;" : lines[i];
+      html = html + '<div class="line">' + line + '</div>';
+    }
+
+    return html;
+  }
+
+
+  /* Return the string contents stored in localStorage.  Uses the localStorage
+   * key defined for this project. If content is undefined, initializes it with
+   * a unicode cursor character and returns that.
+   *
+   * If `splitAtCursor` is True, return an array of 2 strings representing
+   * content before and after the cursor.
+   */
+  function getContent(splitAtCursor) {
     var content = localStorage.getItem(contentKey);
+    if (content === null || content === "") {
+      // initialize the content
+      content = CURSOR;
+      setContent(content);
+    }
+
+    if (splitAtCursor) {
+      var parts = content.split(CURSOR);
+      if (parts.length != 2) {
+        parts[0] = content;
+        parts[1] = "";
+      }
+      return parts;
+    }
+
     return content;
   }
 
 
-  function insertStringAt(s, orig, index) {
-    if (index === undefined) {
-      index = orig.length;
-    }
-    var before = orig.substring(0, index);
-    var after = orig.substring(index);
-    return before + s + after;
+  function setContent(content) {
+    localStorage.setItem(contentKey, content);
   }
 
-  /* Add a character to localStorage at the specified position `index`, or
-   * append at end.
-   * `index` is a zero-based integer and corresponds to the position of the
-   * cursor within the content.
+
+  /* Add a character to localStorage at the position of the cursor.
    */
-  function addChar(c, index) {
-    // either load from localStorage or use what's already in #content
-    var content = loadContent();
-
-    saveContent(insertStringAt(c, content, index));
-    drawContent();
+  function addChar(c) {
+    var parts = getContent(true);
+    setContent(parts[0] + c + CURSOR + parts[1]);
   }
 
+
+  function handlePrintableKeys(e) {
+    console.log('body keypress (printable)');
+    console.log(e.which, ' == ', String.fromCharCode(e.which));
+
+    // if it's a visible character, store it and draw it
+    if (e.which == ENTER || e.which == BACK_SPACE) {
+      return;
+    }
+    addChar(String.fromCharCode(e.which));
+    refreshContent();
+  }
+
+
+  function handleNonPrintableKeys(e) {
+    console.log('body keydown: ', e.which);
+    var parts = getContent(true);
+    var before = parts[0];
+    var after = parts[1];
+
+    function moveCursorLeft() {
+      // setContent(content.substring(0, content.length - 1));
+    }
+
+    switch (e.which) {
+      case ENTER:
+        setContent(before + LS + CURSOR + after);
+        refreshContent();
+        break;
+
+      case BACK_SPACE:
+        // remove the character just before the cursor
+
+        setContent(before.substring(0, before.length - 2) + CURSOR + after);
+        refreshContent();
+        break;
+
+      case DELETE:
+        // remove the character just after the cursor
+        break;
+
+      case RIGHT:
+        before = before + after.substring(0, 1);
+        after = after.substring(1);
+        setContent(before + CURSOR + after);
+        refreshContent();
+        break;
+
+      case LEFT:
+        after = before.substring(before.length - 1) + after;
+        before = before.substring(0, before.length - 1);
+        setContent(before + CURSOR + after);
+        refreshContent();
+        break;
+    }
+  }
+
+
+  function adjustCursorPosition(e) {
+    console.log('.page click');
+
+    var index = 0;
+    // translate the x,y coords into index
+  }
+
+
+  function setupHandlers() {
+    $(document).on('keypress', handlePrintableKeys);
+    $(document).on('keydown', handleNonPrintableKeys);
+    $('.page').on('click', adjustCursorPosition);
+  }
+
+
+  function run() {
+    setupHandlers();
+    refreshContent();
+  }
+
+  // Public functions
   return {
-    addChar: addChar,
-    drawContent: drawContent,
-    loadContent: loadContent
+    run: run
   };
 })();
 
 
 $(document).ready(function() {
-  type.drawContent(type.loadContent());
-});
-
-
-$(document).on('keypress', function(e) {
-  console.log('body keypress (printable)');
-
-  // if it's a visible character, store it and draw it
-  console.log(e.which, ' == ', String.fromCharCode(e.which));
-  // type.addChar(String.fromCharCode(e.which));
-});
-
-
-$(document).on('keydown', function(e) {
-  console.log('body keydown');
-
-  // if it's a visible character, store it and draw it
-  console.log(e.which);
-});
-
-
-$('.page').on('click', function(e) {
-  console.log('.page click');
+  type.run();
 });
